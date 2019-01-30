6082b87c9e37a2d44dee9fc2cf78a08fda2ff912
johnaclouse <johnaclouse@gmail.com>
2019-01-29 21:53:19 -0800

    Added documentation for adjectives and animals data.

---
+++ R/adjectives.R
@@ -0,0 +1,17 @@
+#' Adjectives.
+#'
+#' Select English language adjectives.
+#'
+#' @format A vector containing 214 adjectives
+#' @source <https://github.com/rstudio/cranwhales>
+"adjectives"
+
+#' @name adjectives
+#' @title Adjectives
+#' @description A vector containing 214 adjectives
+#' @docType data
+#' @usage adjectives
+#' @format vector
+#' @source https://github.com/rstudio/cranwhales
+#' @author Stolen from Joe Cheng
+#' @export
---
+++ R/animals.R
@@ -0,0 +1,17 @@
+#' Animals.
+#'
+#' Select English language animals.
+#'
+#' @format A vector containing 178 animals
+#' @source <https://github.com/rstudio/cranwhales>
+"animals"
+
+#' @name animals
+#' @title animals
+#' @description A vector containing 178 animals
+#' @docType data
+#' @usage animals
+#' @format vector
+#' @source https://github.com/rstudio/cranwhales
+#' @author Stolen from Joe Cheng
+#' @export
