0cca2c672964deb638e74b2aa0988e275610f8f0
johnaclouse <johnaclouse@gmail.com>
2019-02-15 22:36:06 -0800

    Added plot_ggpairs in addition to package maintenance changes.

--- R/adjective_animal.R
+++ R/adjective_animal.R
@@ -1,5 +1,18 @@
+#' Return a name comprised of an adjective animal word pair
+#'
+#' @name adjective_animal
+#'
+#' @param n number of word pairs to return
+#'
+#' @return vector of character values containting adjective_animal names
+#' @export
+#'
+#' @examples
+#' adjective_animal(3)
+
 data('adjectives')
 data('animals')
+
 adjective_animal <- function(n = 1) {
 x <- sample(length(adjectives) * length(animals), n * 2, replace = TRUE)
 adj <- x[seq_along(x) %% 2 == 1] %% length(adjectives) + 1
@@ -7,4 +20,3 @@ ani <- x[seq_along(x) %% 2 == 0] %% length(animals) + 1
 paste0(adjectives[adj], "_", animals[ani])
 }
 
-# adjective_animal()
